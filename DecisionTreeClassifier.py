81246134c5fed6876c380a5464d858b03b1ab975
Fardin <syedfardin0612@gmail.com>
2025-04-27 20:14:45 +0530

    Found LL and UL of ith feature

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -17,9 +17,6 @@ def PrintMatrix(matrix,m,n):
             print(str(matrix[i][j])+"  ",end="")
         print("")
 
-def Classify(train_set ,label_set, m, n):
-    pass
-
 def MakeRandMatrix(m,n,a,b):
     # here order of matrix is m*n and range of the elements is a to b (both inclusive)
     matrix = []
@@ -35,6 +32,36 @@ def MakeRandMatrix(m,n,a,b):
 
     return matrix
 
-matrix = MakeRandMatrix(2,2,1,10)
-print(matrix)
-PrintMatrix(matrix,2,2)
+def Classify(train_set ,label_set, m, n):
+    # Decision tree matrix format:
+    # [.......]
+    # one element -> "p{'GE'or'LE}qr" where p -> index of the feature
+                                            # q -> value
+                                            # r -> label
+    # if the if statement is true then r label is returned or else we go to the next if statement
+    # if none satisfied we will return 2 for debugging
+
+    des_tree = []
+    matrix=train_set
+    labels=label_set
+
+    while True:
+        #we break when the whole matrix is labelled the same
+
+        #how to check if all the elements of an array are identical
+        for i in des_tree:
+            if(labels[1]!=labels[i]):
+                break
+
+        for i in range(0,n):
+            # finding the lower and upper limits in ith feature
+            LL = matrix[0][i]
+            UL = matrix[0][i]
+            for a in range(0,len(matrix)):
+                if(matrix[a][i]>UL):
+                    matrix[a][i] = UL
+            for a in range(0,len(matrix)):
+                if(matrix[a][i]<LL):
+                    matrix[a][i] = LL
+
+            #Now we go from 
