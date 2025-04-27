b3ef939aab442a044803f609d891bb8a5c57b48d
Fardin <syedfardin0612@gmail.com>
2025-04-27 19:10:43 +0530

    Initial comments written

---
+++ DecisionTreeClassifier.py
@@ -0,0 +1,11 @@
+from random import randint
+
+
+# We will write 2 functions:
+# 1 -> The classifier which takes an m*n matrix. 
+#     m -> number of entries
+#     n-1 -> features
+#     1 -> label(0 or 1)
+
+#     returns a decision tree which is a matrix
+# 2 -> The result which takes in two matrix. A decision tree and a question matrix of order 1*(n-1) and returns a label 0 or 1
