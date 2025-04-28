2f76859c7c3b4c884040ed5627e2802bc669583c
Fardin <syedfardin0612@gmail.com>
2025-04-28 17:00:26 +0530

    Dint write fact code

--- FibbRecursion.py
+++ FibbRecursion.py
@@ -1,7 +1,25 @@
 def fact(n):
     if n==1:
         return 1
-    
     return n*fact(n-1)
+# 5! = 5*4!
 
-print(fact(5))
+def fibb(n):
+
+
+#fibb(7) = 0+1+1+2+3+5+8 =  fibb(6) + 
+#fibb(6)=  0+1+1+2+3+5 
+#fibb(5)=0+1+1+2+3
+
+    if(n==1):
+        return 0
+    elif(n==2):
+        return 1
+    else:
+        return 1+
+
+
+ 
+
+# print(fact(5))
+fibb(1)
